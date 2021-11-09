455144de32708fb6e9f8df71d1bc2195851381c4
Preksha Gandhi <prekshagandhi@gmail.com>
2021-11-09 16:43:43 -0500

    Routes are working

--- models/workout.js
+++ models/workout.js
@@ -3,39 +3,39 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Exercise type:",
-  },
-  name: {
-    type: String,
-    required: "Exercise name:",
-  },
-  distance: {
-    type: Number,
-    required: "Distance:",
-  },
-  duration: {
-    type: Number,
-    required: "Duration:",
-  },
-  weight: {
-    type: Number,
-    required: "Weight:",
-  },
-  sets: {
-    type: Number,
-    required: "Sets:",
-  },
-  reps: {
-    type: Number,
-    required: "Reps:",
-  },
-  date: {
+  day: {
     type: Date,
     default: Date.now,
   },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Exercise type:",
+      },
+      name: {
+        type: String,
+        required: "Exercise name:",
+      },
+      distance: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: "Duration:",
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,57 +1,78 @@
-
+const path = require('path')
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/Workout.js");
 
-router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-      .sort({ date: -1 })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+})
 
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkout => {
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+})
+
+router.get("/api/workouts/range", ({body}, res) => {
+  Workout.aggregate([
+    {
+        $addFields: {
+          totalDuration: {
+              $sum: "$exercises.duration"
+          }
+        }
+    }
+    ])
+    .sort({ day: -1 })
+    .limit(7)
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-router.put("/api/workouts/:id", ({ body }, res) => {
-  Workout.updateOne(
-    {
-      _id: mongojs.ObjectId(params.id)
-    },
-    {
-      $set: {...body}
-    },
-
-    (error, edited) => {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        console.log(edited);
-        res.send(edited);
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+      {
+          $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration"
+            }
+          }
       }
-    }
-  );
-});
-
-
-router.get("/api/workouts/range", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
+  ])
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.updateOne(
+    {
+      _id: req.params.id
+    },
+    {
+      $push: {
+        exercises: req.body,
+      }
+    }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
